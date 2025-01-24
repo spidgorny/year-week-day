3372d3d34f5071eed80873bb0de13d9c52bbe6a8
Slawa <spidgorny@gmail.com>
2025-01-24 19:21:20 +0100

    build: copy public to standalone

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@sequelize/core"],
   transpilePackages: ["spidgorny-react-helpers"],
+  output: "standalone",
 };
 
 export default nextConfig;
